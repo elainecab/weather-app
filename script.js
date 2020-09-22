4dfa84476561078dc95fd8751d8448db6845baae
Elaine <elaine.cabral@zumba.com>
2020-09-22 17:55:39 -0400

    updating humidity and wind speed

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let speed = document.querySelector("#speed");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  speed.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "a923e76b795634a6870a9f9aaf2bbb9a";
